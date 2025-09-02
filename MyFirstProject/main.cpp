190d96b8fc91bdc6cae05aa76816498407e94fa5
jordynatwood23 <jordynatwood23@gmail.com>
2025-09-02 10:38:00 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,20 @@
+/*
+Jordyn Atwood
+C++ Fall 2025
+Due: 09/09/25
+Lab 1 introduction to C++ programming
+Demonstrate ability to download a project from Github, compile and run the project, 
+upload a project to Github, and submit a project to Canvas
+
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Pink" << std::endl;
+
+	return 0;
+}
+
